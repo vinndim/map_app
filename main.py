65ae4cd87c2d8b4007fb4bb24e3214b671c18f88
Ivan <uvanko1147@gmail.com>
2022-02-02 12:04:19 +0500

    ez layers

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import io
 import pygame as pg
+import pygame_widgets
+
 from search_map_utils import get_organization, org_coord
 from static_api_utils import get_static
+from pygame_widgets.combobox import ComboBox
 
+pg.init()
 SIZE = (600, 450)
+sloi = {'map': 'map', 'sat': 'sat', 'skl': 'skl'}
 
 
 class MapApp:
-    def __init__(self, coord, z=10):
+    def __init__(self, coord):
         self.coord = coord
         self.z = 18
         self.map = None
-        self.update_map()
+        self.update_map('map')
 
-    def update_map(self):
+    def update_map(self, l='map'):
         params = dict(
             ll=','.join(map(str, self.coord)),
             z=self.z,
-            l='map'
+            l=l
         )
         self.map = io.BytesIO(get_static(**params))
 
@@ -38,13 +43,22 @@ class MapApp:
 def main():
     address = 'миасс ддт юность'
     coord = org_coord(get_organization(address))
-    app = MapApp(coord)
-
     pg.init()
     screen = pg.display.set_mode(SIZE)
+    comboBox = ComboBox(
+        screen, 0, 0, 200, 25, name='Select Color',
+        choices=sloi,
+        maxResults=3,
+        font=pg.font.SysFont('calibri', 15),
+        borderRadius=15, colour=(255, 255, 255), direction='down',
+        textHAlign='left'
+    )
+    app = MapApp(coord)
+
     running = True
     while running:
-        for event in pg.event.get():
+        events = pg.event.get()
+        for event in events:
             if event.type == pg.QUIT:
                 running = False
             elif event.type == pg.KEYDOWN:
@@ -68,8 +82,12 @@ def main():
                     if coord[0] > -175:
                         coord[0] -= 0.5
                     print(coord)
-        app.update_map()
+        if comboBox.getText() in ['map', 'sat', 'skl']:
+            app.update_map(comboBox.getText())
+        else:
+            app.update_map()
         app.draw(screen)
+        pygame_widgets.update(events)
         pg.display.update()
 
 
